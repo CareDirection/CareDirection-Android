a3933ac69ca290ebfa9a546861d1b3ce285bb2c9
Choong <cndqjacndqja@naver.com>
2019-12-24 04:29:45 +0900

    shopping directory 추가

---
+++ app/src/main/java/com/example/caredirection/home/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.caredirection.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.caredirection.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_activity)
+
+    }
+}
