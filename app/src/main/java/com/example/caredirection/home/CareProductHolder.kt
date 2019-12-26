dff92cf1f2560404dcecbe84a3f23c97bd53aa90
EuneeChung <chungee375@gmail.com>
2019-12-26 10:46:42 +0900

    recycler view

---
+++ app/src/main/java/com/example/caredirection/home/CareProductHolder.kt
@@ -0,0 +1,8 @@
+package com.example.caredirection.home
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CareProductHolder(view:View) : RecyclerView.ViewHolder(view){
+
+}
