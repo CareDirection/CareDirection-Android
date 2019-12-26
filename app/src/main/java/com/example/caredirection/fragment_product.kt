31bf3b636788b2607d1a8db1d6333b7c52557f4e
Choong <cndqjacndqja@naver.com>
2019-12-26 15:50:03 +0900

    shopping/standard 세가지 성분 카드뷰 추가

--- app/src/main/java/com/example/caredirection/fragment_product.kt
+++ app/src/main/java/com/example/caredirection/fragment_product.kt
@@ -1,7 +1,5 @@
 package com.example.caredirection
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -28,7 +26,7 @@ class fragment_product : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_fragment_product, container, false)
+        val view = inflater.inflate(R.layout.fragment_product, container, false)
 
         return view
     }
