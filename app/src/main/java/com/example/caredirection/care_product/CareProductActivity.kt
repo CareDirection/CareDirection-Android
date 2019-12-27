9bca5e8fffe229d0b186a0ebeea4f2d26f23305d
EuneeChung <chungee375@gmail.com>
2019-12-28 04:55:26 +0900

    복용 제품 등록 rv_item.xml 완성

---
+++ app/src/main/java/com/example/caredirection/care_product/CareProductActivity.kt
@@ -0,0 +1,13 @@
+package com.example.caredirection.care_product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.caredirection.R
+
+class CareProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_care_product)
+    }
+}
