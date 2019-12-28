ac9556d46dca93e0b06711c07cdf6652bd61741c
EuneeChung <chungee375@gmail.com>
2019-12-28 21:20:24 +0900

    복용 제품 등록 view 버튼 추가

--- app/src/main/java/com/example/caredirection/care_product/CareProductActivity.kt
+++ app/src/main/java/com/example/caredirection/care_product/CareProductActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.caredirection.R
 import com.example.caredirection.data.RvCareProductRegisterData
@@ -18,10 +19,17 @@ class CareProductActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_care_product)
 
+
+
         rvCareRegisterView=findViewById(R.id.rv_care_register_view)
+
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(rvCareRegisterView)
+
         rvCareRegisterView.layoutManager=LinearLayoutManager(this)
         rvCareRegisterAdapter= CareProductRegisterAdapter(this)
         rvCareRegisterView.adapter=rvCareRegisterAdapter
+        snapHelper.attachToRecyclerView(rvCareRegisterView)
 
         rvCareRegisterAdapter.data= listOf(
             RvCareProductRegisterData("10외 남음","","회사","해외직구","뉴트리디데이 프리미엄 오메가 3골드 1100",2222222,300,30),
@@ -29,6 +37,7 @@ class CareProductActivity : AppCompatActivity() {
             RvCareProductRegisterData("10외 남음","","회사","해외직구","이건 유산균이다",2222222,300,30),
             RvCareProductRegisterData("10외 남음","","회사","해외직구","이건 유산균이다",2222222,300,30)
         )
+
         rvCareRegisterAdapter.notifyDataSetChanged()
 
 
