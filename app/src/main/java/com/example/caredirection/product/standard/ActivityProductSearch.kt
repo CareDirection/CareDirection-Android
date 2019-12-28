acf3721d9d0ae640da5173b7633465c9faaf17be
Choong <cndqjacndqja@naver.com>
2019-12-28 21:21:48 +0900

    Merge branches 'choong/shopping/standard' and 'eunee/Home' of https://github.com/CareDirection/CareDirection-Android into choong/shopping/standard
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/fragment_product.xml
    #	app/src/main/res/values/colors.xml
    
    chooung - product/search
            - product/standard 뷰 디벨롭, 연결
            - product/standard 뷰 dialog 추가

--- app/src/main/java/com/example/caredirection/product/standard/ActivityProductSearch.kt
+++ app/src/main/java/com/example/caredirection/product/standard/ActivityProductSearch.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Adapter
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.caredirection.R
+import kotlinx.android.synthetic.main.activity_product_search.*
+import kotlinx.android.synthetic.main.dialog_product_standard_explation.view.*
 
 class ActivityProductSearch : AppCompatActivity() {
 
@@ -17,6 +21,9 @@ class ActivityProductSearch : AppCompatActivity() {
         setContentView(R.layout.activity_product_search)
 
         initList()
+        activity_product_cardview_standard1_txt.setOnClickListener{
+            contentDialog()
+        }
     }
     private fun initList(){
 //1. 어뎁터 데이터에 에드시키기 , 2. 리사이클러뷰 가져오기 , 리사이클러뷰 리니얼 설정, 리사이클러뷰에,adapter에 어뎁터 넣기
@@ -39,5 +46,17 @@ class ActivityProductSearch : AppCompatActivity() {
         //리사이클러뷰에 어뎁더 써서 연결하기
         rv_main_product.adapter = rv_main_product_adapter
 
+
+    }
+    private fun contentDialog(){
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_product_standard_explation, null)
+
+
+        builder.setView(dialogView).show()
+
+        dialogView.txt_dialog_product_standard_close.setOnClickListener{
+            finish()
+        }
     }
 }
