25470d260d5a122e0646bb63d2e22d9f671d53f2
Choong <cndqjacndqja@naver.com>
2019-12-31 06:47:57 +0900

    로그인 뷰 회원가입 뷰 추가

--- app/src/main/java/com/example/caredirection/ActivityLogin.kt
+++ app/src/main/java/com/example/caredirection/ActivityLogin.kt
@@ -1,7 +1,9 @@
 package com.example.caredirection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class ActivityLogin : AppCompatActivity() {
 
@@ -9,5 +11,8 @@ class ActivityLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        btn_activity_register.setOnClickListener{
+            startActivity(Intent(this@ActivityLogin, ActivityRegister::class.java))
+        }
     }
 }
