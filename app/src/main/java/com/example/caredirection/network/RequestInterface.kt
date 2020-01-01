e6434ecde1c6f11ae4103a7eb5d6950fb139c8a5
EuneeChung <chungee375@gmail.com>
2020-01-02 06:40:02 +0900

    기능성 원료 - data class, interface

--- app/src/main/java/com/example/caredirection/network/RequestInterface.kt
+++ app/src/main/java/com/example/caredirection/network/RequestInterface.kt
@@ -1,5 +1,6 @@
 package com.example.caredirection.network
 
+import com.example.caredirection.data.network.FunctionalData
 import com.example.caredirection.data.network.IngredientData
 import com.example.caredirection.data.network.LoginData
 import retrofit2.Call
@@ -13,10 +14,17 @@ interface RequestInterface {
         @Field("user_id")user_id : String //category에 String 타입 category 값 전달.
     ) : Call<LoginData>
 
+    //성분학습뷰 -성분 학습 - 은이
     @GET("/nutrient/{nutrient_idx}")
     fun getIngredientInfo(
         @Header("token")token: String,
         @Path("nutrient_idx")nutrient_idx:Int
     ):Call<IngredientData>
+
+    //홈뷰 - 기능성 원료 - 은이
+    @GET("/nutrient/function")
+    fun getFunctional(
+        @Header("token")token: String
+    ):Call<FunctionalData>
 
 }
