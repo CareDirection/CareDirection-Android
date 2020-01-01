51b59089cd62c12b64585a768fe8eb8a0fa22d28
EuneeChung <chungee375@gmail.com>
2020-01-01 18:04:17 +0900

    recycler view item click

--- app/src/main/java/com/example/caredirection/study/ingredient/IngredientAdapter.kt
+++ app/src/main/java/com/example/caredirection/study/ingredient/IngredientAdapter.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.caredirection.R
 import com.example.caredirection.data.RvIngredientData
-import com.example.caredirection.home.CareProductHolder
 
 class IngredientAdapter (private val context: Context):RecyclerView.Adapter<IngredientHolder>(){
 
