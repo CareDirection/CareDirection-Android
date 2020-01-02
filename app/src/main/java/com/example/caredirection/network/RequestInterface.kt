d2242bf3d279a8798ef825c93412051ef93f009e
Haeeul <etoilune00@gmail.com>
2020-01-03 08:53:45 +0900

    id중복확인,회원가입,로그인 통신 완료 (로그인은 서버쪽 점검 필요, 요청완료) 자동로그인 구현 완료

--- app/src/main/java/com/example/caredirection/network/RequestInterface.kt
+++ app/src/main/java/com/example/caredirection/network/RequestInterface.kt
@@ -1,16 +1,34 @@
 package com.example.caredirection.network
 
+import com.example.caredirection.data.network.IdData
 import com.example.caredirection.data.network.IngredientData
 import com.example.caredirection.data.network.LoginData
+import com.example.caredirection.data.network.SignUpData
 import retrofit2.Call
 import retrofit2.http.*
 
 interface RequestInterface {
+    // 로그인 뷰 - 중복확인 - 명희
+    @FormUrlEncoded
+    @POST("/users/id")
+    fun postIdCheck(
+        @Field("user_id")user_id : String
+    ) : Call<IdData>
 
+    // 로그인 뷰 - 회원가입 - 명희
+    @FormUrlEncoded
+    @POST("/users/signup")
+    fun postSignUp(
+        @Field("user_id")user_id : String,
+        @Field("user_pw")user_pw : String
+    ) : Call<SignUpData>
+
+    // 로그인 뷰 - 로그인 - 명희
     @FormUrlEncoded
     @POST("/users/signin") // uri
     fun postLogin(
-        @Field("user_id")user_id : String //category에 String 타입 category 값 전달.
+        @Field("user_id")user_id : String,
+        @Field("user_pw")user_pw : String
     ) : Call<LoginData>
 
     @GET("/nutrient/{nutrient_idx}")
@@ -18,5 +36,7 @@ interface RequestInterface {
         @Header("token")token: String,
         @Path("nutrient_idx")nutrient_idx:Int
     ):Call<IngredientData>
+
+
 
 }
