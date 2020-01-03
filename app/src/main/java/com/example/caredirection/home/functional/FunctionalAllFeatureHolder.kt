134cb2b79ca9bc09a3707e629c25a88d3b22a16e
seunghee63 <0603yang@naver.com>
2020-01-04 08:49:13 +0900

    Add efficacystudy api & Modify detailed

--- app/src/main/java/com/example/caredirection/home/functional/FunctionalAllFeatureHolder.kt
+++ app/src/main/java/com/example/caredirection/home/functional/FunctionalAllFeatureHolder.kt
@@ -14,8 +14,6 @@ import com.example.caredirection.study.ActivityStudySymptom
 class FunctionalAllFeatureHolder(view: View, var context: Context) : RecyclerView.ViewHolder(view){
     val imgFunctionalAll: ImageView = view.findViewById(R.id.img_rv_item_functional_all)
     val txtFunctionalAll : TextView = view.findViewById(R.id.txt_rv_item_functional_all)
-    val txtlist= listOf("간건강","피로회복","눈건강","혈행개선","면역력 황산화","소화기능","두뇌활동","운동보조","뼈")
-
 
     fun bind(data: RvFunctionalAllData){
         imgFunctionalAll.setImageResource(data.img)
@@ -23,7 +21,7 @@ class FunctionalAllFeatureHolder(view: View, var context: Context) : RecyclerVie
 
         itemView.setOnClickListener{
             val intent = Intent(context, ActivityStudySymptom::class.java)
-
+            intent.putExtra("efficacy",txtFunctionalAll.text.toString())
             startActivity(context ,intent, null)
         }
     }
