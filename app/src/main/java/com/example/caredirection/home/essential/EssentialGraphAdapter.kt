5992fa72d883f93f20128fc3cf5393a474fafd38
EuneeChung <chungee375@gmail.com>
2020-01-04 08:47:13 +0900

    - ing

---
+++ app/src/main/java/com/example/caredirection/home/essential/EssentialGraphAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.caredirection.home.essential
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.caredirection.R
+import com.example.caredirection.home.care_product.CareProductHolder
+
+class EssentialGraphAdapter(private val context: Context) : RecyclerView.Adapter<EssentialGraphHolder>(){
+
+
+    override fun onBindViewHolder(holder: EssentialGraphHolder, position: Int) {
+//        val view = LayoutInflater.from(context).inflate(R.layout.rv_item_,parent,false)
+//        return EssentialGraphHolder(view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EssentialGraphHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ app/src/main/java/com/example/caredirection/home/essential/EssentialGraphHolder.kt
@@ -0,0 +1,10 @@
+package com.example.caredirection.home.essential
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class EssentialGraphHolder (view: View):RecyclerView.ViewHolder(view){
+
+
+
+}
